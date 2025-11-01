18045cab2fe108ad4cdbe5180e0634e417542108
Max Ma <thedealnavigator.save@gmail.com>
2025-11-01 07:21:02 -0600

    Add rewrite rule from /go/:id → /api/go/:id

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/go/:id',
+        destination: '/api/go/:id',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
